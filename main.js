c4685a35fe080a83ad2323e5e6df782893f2d97a
George Sandoval <george.sandoval.estela@gmail.com>
2022-07-11 09:19:10 -0500

    feat: add numbers & operations

---
+++ main.js
@@ -0,0 +1,12 @@
+let sum = false
+let substract = false
+let multiply = false
+let division = false
+
+
+let inputText = document.getElementById("input")
+
+
+function insertElement(element){
+    inputText.innerHTML += element
+}
